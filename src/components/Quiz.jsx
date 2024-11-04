3a58c4f030ece318056a27aeac8ae6f24233cce9
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-04 18:29:07 -0300

    feat: wrap Quiz component in main tag and add answer selection functionality

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,16 +6,26 @@ const Quiz = () => {
 
   const activeQuestionIndex = userAnswers.length;
 
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
+
   return (
-    <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li>
-            <button>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };

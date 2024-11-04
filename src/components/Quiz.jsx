25b1ca70fbb9a14bfb4dcc55dc6dbc7c4f4122e9
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-04 18:23:51 -0300

    feat: display active question and answers in Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
+import QUESTIONS from '../questions.js';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
-  return <p>JS Quiz</p>;
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li>
+            <button>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Quiz;

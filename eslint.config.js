479c668a7f896f499268b98218ff77786e0796b6
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-04 20:06:41 -0300

    feat: add globals support in ESLint configuration and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   js.configs.recommended, // Configuración recomendada de ESLint para JavaScript básico
@@ -11,17 +12,20 @@ export default [
       'react-hooks': reactHooks, // Habilitar el plugin de React Hooks
     },
     languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: 'readonly',
+      },
       parserOptions: {
         ecmaVersion: 2021,
         sourceType: 'module',
+
         ecmaFeatures: {
           jsx: true,
         },
       },
     },
-    rules: {
-      'react-hooks/rules-of-hooks': 'error', // Reglas básicas para hooks
-      'react-hooks/exhaustive-deps': 'warn', // Aviso para dependencias faltantes en useEffect
-    },
+    rules: reactHooks.configs.recommended.rules,
   },
 ];

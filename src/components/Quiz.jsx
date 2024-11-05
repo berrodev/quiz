af76e3b2593e53be82bb89153b9d8cabfe14de8e
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-05 14:11:38 -0300

    feat: add key prop to Answers component for improved rendering on question change

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -62,6 +62,7 @@ const Quiz = () => {
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <Answers
+          key={activeQuestionIndex}
           answers={QUESTIONS[activeQuestionIndex].answers}
           selectedAnswer={userAnswers[userAnswers.length - 1]}
           answerState={answerState}
